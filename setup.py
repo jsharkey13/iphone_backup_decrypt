7351d2193a5be47707c211fee8b3e616fe2286c0
James Sharkey <jsharkey13@users.noreply.github.com>
2020-03-12 16:47:16 +0000

    Increment version to reflect important bug fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='iphone_backup_decrypt',
-      version='0.2',
+      version='0.2.1',
       description='Decrypt an iOS13+ encrypted local backup.',
       url='https://github.com/jsharkey13/iphone_backup_decrypt',
       author='James Sharkey',
